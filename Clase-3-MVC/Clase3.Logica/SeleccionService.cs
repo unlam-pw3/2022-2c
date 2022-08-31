65de68880f0bc5dc0ee567e93366e370f166342c
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2022-08-31 20:42:35 -0300

    Agregando seccion selecciones (selecciones/index) y listar Todas las selecciones (selecciones/Todas)

---
+++ Clase-3-MVC/Clase3.Logica/SeleccionService.cs
@@ -0,0 +1,29 @@
+﻿using Clase3.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase3.Logica
+{
+    public class SeleccionService
+    {
+        private static List<Seleccion> Selecciones { get; set; } = new List<Seleccion>() 
+        {
+            new Seleccion() { Pais = "Argentina", Clasificada = true, Continente = "America"},
+            new Seleccion() { Pais = "Brasil", Clasificada = true, Continente = "America"},
+            new Seleccion() { Pais = "Italia", Clasificada = false, Continente = "Europa"},
+        };
+
+        public List<Seleccion> ObtenerTodas()
+        {
+            return Selecciones;
+        }
+
+        public List<Seleccion> ObtenerClasificados()
+        {
+            return Selecciones.Where(o => o.Clasificada).ToList();
+        }
+    }
+}
---
+++ Clase-3-MVC/Clase3.Web/Controllers/SeleccionesController.cs
@@ -0,0 +1,21 @@
+﻿using Clase3.Entidades;
+using Clase3.Logica;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clase3.Web.Controllers
+{
+    public class SeleccionesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Todas()
+        {
+            SeleccionService seleccionService = new SeleccionService();
+            List<Seleccion> todasSelecciones = seleccionService.ObtenerTodas();
+            return View(todasSelecciones);
+        }
+    }
+}
