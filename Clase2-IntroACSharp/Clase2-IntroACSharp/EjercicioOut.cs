6fc1229f2310b5c654305183345dcf78c7f82996
Nahuel Rapetti <n.rapetti@hotmail.com>
2022-09-07 19:23:57 -0300

    Clase 2 (#14)
    Co-authored-by: Nahuel Rapetti <nahuelomar.rapetti@mercadolibre.com>

---
+++ Clase2-IntroACSharp/Clase2-IntroACSharp/EjercicioOut.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace Clase2_IntroACSharp
+{
+    public class EjercicioOut
+    {
+        public EjercicioOut()
+        {
+        }
+
+        public static void Ejecutar()
+        {
+            Console.WriteLine("Ingrese su nombre:");
+            string nombre = Console.ReadLine();
+
+            string resultadoU;
+            string resultadoL;
+
+            ConvertirEnMayusculas(nombre, out resultadoL, out resultadoU);
+
+            // ConvertirEnMayusculasSinOut(nombre);
+
+            Console.WriteLine("En mayusculas:");
+            Console.WriteLine(resultadoU);
+            Console.WriteLine("En minusculas:");
+            Console.WriteLine(resultadoL);
+            Console.ReadLine();
+        }
+
+        private static bool ConvertirEnMayusculas(string nombre, out string resultadoLower, out string resultadoUpper)
+        {
+            if (!string.IsNullOrEmpty(nombre))
+            {
+                resultadoUpper = nombre.ToUpper();
+                resultadoLower = nombre.ToLower();
+                return true;
+            }
+            else
+            {
+                resultadoUpper = "Nombre vacío";
+                resultadoLower = "Nombre vacío";
+                return false;
+            }
+        }
+
+        private static string ConvertirEnMayusculasSinOut(string nombre)
+        {
+            if (!string.IsNullOrEmpty(nombre))
+            {
+                return nombre.ToLower();
+            }
+            else
+            {
+                return "Nombre vacio";
+            }
+        }
+    }
+}
+
---
+++ Clase2-IntroACSharp/Clase2-IntroACSharp/EjercicioRef.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Clase2_IntroACSharp
+{
+    public class EjercicioRef
+    {
+        public static void Ejecutar()
+        {
+            Console.WriteLine("Ingrese su nombre:");
+            string nombre = Console.ReadLine();
+
+            //lo cambio necesito otro nombre
+            CambiarNombre(ref nombre);
+
+            Console.WriteLine("Nombre cambiado:");
+            Console.WriteLine(nombre);
+            Console.ReadLine();
+        }
+
+        private static void CambiarNombre(ref string nombre)
+        {
+            nombre = "Otro Nombre";
+        }
+    }
+}
+
