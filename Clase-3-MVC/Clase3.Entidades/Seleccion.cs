db893c06ee968a059822918fd8ceb0854e8b2908
Juanchini23 <91612292+Juanchini23@users.noreply.github.com>
2022-09-14 19:41:39 -0300

    Ejercicio MVC de selecciones (#13)
    * ejercicio mvc
    
    * Update SeleccionService.cs
    
    Co-authored-by: pablokuko <pablokuko@gmail.com>

--- Clase-3-MVC/Clase3.Entidades/Seleccion.cs
+++ Clase-3-MVC/Clase3.Entidades/Seleccion.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +12,6 @@ namespace Clase3.Entidades
         public string Pais { get; set; } = string.Empty;
         public bool Clasificada { get; set; }
         public string Continente { get; set; } = string.Empty;
+        public string Confederacion { get; set; } = string.Empty;
     }
 }
